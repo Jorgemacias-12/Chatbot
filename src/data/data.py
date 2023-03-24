ece584527edf3ffe80b130eb6e2b00050d45949a
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-03-24 16:22:22 -0600

    Add: preguntas añadidas

---
+++ src/data/data.py
@@ -0,0 +1,83 @@
+questions = {
+    1: {
+        "pregunta": "¿Prefieres trabajar en equipo o en solitario?",
+        "opciones": ["a) En equipo", "b) En solitario", "c) Depende de la situación", "d) No estoy seguro"]
+    },
+    2: {
+        "pregunta": "¿Qué animal te indentifica mejor?",
+        "opciones": ['a) León', 'b) Zorro', 'c) Delfín', 'd) Águila']
+    },
+    3: {
+        "pregunta": "¿Eres más de seguir las reglas o de cuestionarlas?",
+        "opciones": ['a) Seguir las reglas', 'b) Cuestionar las reglas', 'c) Ambas, dependiendo la situación', 'd) No estoy seguro']
+    },
+    4: {
+        "pregunta": "¿Qué actividad te gusta más?",
+        "opciones": ['a) Resolver problemas matemáticos', 'b) Leer y escribir', 'c) Dibujar o pintar', 'd) Hacer deporte']
+    },
+    5: {
+        "pregunta": "¿Te gusta la tecnología o la informática?",
+        "opciones": ['a) Sí, me apasiona', 'b) No me interesa demasiado', 'c) Me interesa, pero no tanto como otras cosas', 'd) No estoy seguro']
+    },
+    6: {
+        "pregunta": "¿Prefieres trabajar en un lugar tranquilo o con mucha actividad?",
+        "opciones": ['a) Lugar tranquilo', 'b) Con mucha actividad', 'c) Depende la situación', 'd) No estoy seguro']
+    },
+    7: {
+        "pregunta": "¿Qué elemento te representa mejor?",
+        "opciones": ['a) Agua', 'b) Tierra', 'c) Aire', 'd) Fuego']
+    },
+    8: {
+        "pregunta": "¿Eres más de planificar o de improvisar?",
+        "opciones": ['a) Planificar', 'b) Improvisar', 'c) Ambas, dependiendo de la situación', 'd) No estoy seguro']
+    },
+    9: {
+        "pregunta": "¿Qué tipo de música prefieres",
+        "opciones": ['a) Clásica', 'b) Rock o pop', 'c) Jazz o blues', 'd) Reggae o música latina']
+    },
+    10: {
+        "pregunta": "¿Te gusta trabajar con las manos?",
+        "opciones": ['a) Sí, me gusta mucho', 'b) No me gusta mucho', 'c) Me gusta, pero no tanto como otras cosas', 'd) No estoy seguro']
+    },
+    11: {
+        "pregunta": "¿Qué prefieres hacer en tu tiempo libre?",
+        "opciones": ['a) Leer o ver películas', 'b) Practicar deportes o hacer ejercicio', 'c) Salir con amigos o familiares', 'd) Otros']
+    },
+    12: {
+        "pregunta": "¿Qué tipo de situaciones te generan mayor ansiedad?",
+        "opciones": ['a) Hablar en público', 'b) Conocer gente nueva', 'c) Trabajar bajo presión', 'd) No suelo sentir ansiedad']
+    },
+    13: {
+        "pregunta": "¿Qué tipo de ambiente de trabajo prefieres?",
+        "opciones": ['a) Formal y estructurado', 'b) Creativo y dinámico', 'c) Relajado y flexible', 'd) No estoy seguro']
+    },
+    14: {
+        "pregunta": "¿Cuál de estas descripciones te describe mejor?",
+        "opciones": ['a) Soy una persona analítica y crítica', 'b) Soy una persona creativa y artística', 'c) Soy una persona empática y comprensiva', 'd) Soy una persona organizada y detallista']
+    },
+    15: {
+        "pregunta": "¿Te gusta aprender sobre ciencias naturales y el mundo que nos rodea?",
+        "opciones": ['a) Sí, me encanta', 'b) No me interesa mucho', 'c) Me interesa, pero no tanto como otras cosas', 'd) No estoy seguro']
+    },
+    16: {
+        "pregunta": "¿Qué tipo de libros o películas te gustan más?",
+        "opciones": ['a) Drama o suspenso', 'b) Ciencia ficción o fantasía', 'c) Comedia o romance', 'd) Documentales o biografías']
+    },
+    17: {
+        "pregunta": "¿Te gusta ayudar a los demás?",
+        "opciones": ['a) Sí, mucho', 'b) No tanto', 'c) Me gusta, pero no tanto como otras cosas', 'd) No estoy seguro']
+    },
+    18: {
+        "pregunta": "¿Eres más de trabajar con la mente o con el cuerpo?",
+        "opciones": ['a) Con la mente', 'b) Con el cuerpo', 'c) Ambos, dependiendo de la situación', 'd) No estoy seguro']
+    },
+    19: {
+        "pregunta": "¿Qué habilidades crees que son tus fortalezas?",
+        "opciones": ['a) Capacidad analítica', 'b) Creatividad', 'c) Empatía', 'd) Organización y atención al detalle']
+    },
+    20: {
+        "pregunta": "¿Te interesa aprender sobre leyes y cómo funcionan los sistemas legales?",
+        "opciones": ['a) Sí, me parece interesante', 'b) No me interesa mucho', 'c) Me interesa, pero no tanto como otras cosas', 'd) No estoy seguro']
+    },
+
+}
