f44da1a61c1d40dcf80cad7055b3b75d621b60ca
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-05-07 01:21:51 -0600

    Add: mejoras al logging
    	He mejorado aún más el sistema de logging de la aplicación,
    	falta por terminar de mejorar las demas interfaces.
    
    	he añadido a su vez un metodo fuera de la clase para
    	llamarlo cuando sea necesario, agregando el formato que
    	me gusta.

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -3,6 +3,7 @@ import logging
 import platform
 import subprocess
 import tkinter as tk
+from tkinter.font import *
 
 from datetime import date
 
@@ -19,14 +20,17 @@ class LoggingTkClass(tk.Tk):
         self.resizable(resizable_x, resizable_y)
         self.minsize(window_width, window_height)
 
-        # # Calculate Frame position
+        # Use fonts
+        self.default_font = Font(family="Arial", size=12, weight="normal")
+        self.title_font = Font(family="Arial", size=14, weight="bold")
+
+        # Calculate Frame position
         window_x_pos = (self.winfo_screenwidth() // 2) - (window_width // 2)
         window_y_pos = (self.winfo_screenheight() // 2) - (window_height // 2)
 
         self.geometry(f"{window_width}x{window_height}+{window_x_pos}+{window_y_pos}")
 
         #  Debug setup
-
         appdata_path = os.getenv("APPDATA")
 
         logger_folder_path = os.path.join(appdata_path, 'com.jamz.orientador-vocacional')
@@ -54,17 +58,28 @@ class LoggingTkClass(tk.Tk):
         operating_system = platform.system()
 
 
-        # Imprimir información del sistema en un formato ordenado
-        self.logger.info("\n")
-        self.logger.info("+{:-^60}+".format(""))
-        self.logger.info("|{:^60}|".format("Organizador Vocacional CUCOSTA inicializado"))
-        self.logger.info("+{:-^60}+".format(""))
-        self.logger.info("|{:^60}|".format("Información del sistema"))
-        self.logger.info("+{:-^60}+".format(""))
-        self.logger.info("| {:<20} | {:<35} |".format("Nombre de usuario", username))
-        self.logger.info("| {:<20} | {:<35} |".format("Procesador", processor_name))
-        self.logger.info("| {:<20} | {:<35} |".format("Arquitectura", architecture))
-        self.logger.info("| {:<20} | {:<35} |".format("Versión del SO", os_version))
-        self.logger.info("| {:<20} | {:<35} |".format(
-    "Sistema Operativo", operating_system))
-        self.logger.info("+{:-^60}+".format(""))
+        # Imprimir información del sistema en un formato ordenado 
+        # Solo si la clase es ChatbotWindow
+        if type(self).__name__ == "ChatbotWindow":
+            self.logger.info("\n")
+            self.logger.info("+{:-^60}+".format(""))
+            self.logger.info("|{:^60}|".format("Organizador Vocacional CUCOSTA inicializado"))
+            self.logger.info("+{:-^60}+".format(""))
+            self.logger.info("|{:^60}|".format("Información del sistema"))
+            self.logger.info("+{:-^60}+".format(""))
+            self.logger.info("| {:<20} | {:<35} |".format("Nombre de usuario", username))
+            self.logger.info("| {:<20} | {:<35} |".format("Procesador", processor_name))
+            self.logger.info("| {:<20} | {:<35} |".format("Arquitectura", architecture))
+            self.logger.info("| {:<20} | {:<35} |".format("Versión del SO", os_version))
+            self.logger.info("| {:<20} | {:<35} |".format(
+        "Sistema Operativo", operating_system))
+            self.logger.info("+{:-^60}+".format(""))   
+        
+        # Imprimir que frame fue inicializado
+        log(self.logger, f"Inicializar frame: {type(self).__name__}")
+
+def log(logger, message):
+
+    logger.info("+{:-^{width}}+".format("", width=len(message) + 25))
+    logger.info("| {:<20} | {:{width}} |".format("Mensaje", message, width=len(message)))
+    logger.info("+{:-^{width}}+".format("", width=len(message) + 25))
