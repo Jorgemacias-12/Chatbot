fdae66de30a20c39d830a83d67653119aaf7a489
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-05-07 00:35:23 -0600

    Add: mejoras al template
    	He mejorado el template grafico, añadiendo la estructura básica
    	en la clase que será tomada para hacer herencia.
    
    	Además de mejorar el sistema de loggeo de la aplicación para
    	guardar la información de mejor forma para evitar confusiones.

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,15 +1,32 @@
 import os 
 import logging
+import platform
+import subprocess
 import tkinter as tk
 
 from datetime import date
 
 class LoggingTkClass(tk.Tk):
     
-    def __init__(self):
+
+
+    def __init__(self, window_width=800, window_height=520, title="Frame", resizable_x=False, resizable_y=False):
         
         super().__init__()
         
+        self.title(title)
+
+        self.resizable(resizable_x, resizable_y)
+        self.minsize(window_width, window_height)
+
+        # # Calculate Frame position
+        window_x_pos = (self.winfo_screenwidth() // 2) - (window_width // 2)
+        window_y_pos = (self.winfo_screenheight() // 2) - (window_height // 2)
+
+        self.geometry(f"{window_width}x{window_height}+{window_x_pos}+{window_y_pos}")
+
+        #  Debug setup
+
         appdata_path = os.getenv("APPDATA")
 
         logger_folder_path = os.path.join(appdata_path, 'com.jamz.orientador-vocacional')
@@ -24,3 +41,30 @@ class LoggingTkClass(tk.Tk):
                             format='%(asctime)s %(levelname)s: %(message)s')
         
         self.logger = logging.getLogger(__name__)
+
+        self.initial_log()
+
+    def initial_log(self):
+
+        username = os.getlogin()
+        processor_name = subprocess.check_output(
+            ["wmic", "cpu", "get", "name"]).decode().strip().split("\n")[1]
+        architecture = platform.architecture()[0]
+        os_version = platform.version()
+        operating_system = platform.system()
+
+
+        # Imprimir información del sistema en un formato ordenado
+        self.logger.info("\n")
+        self.logger.info("+{:-^60}+".format(""))
+        self.logger.info("|{:^60}|".format("Organizador Vocacional CUCOSTA inicializado"))
+        self.logger.info("+{:-^60}+".format(""))
+        self.logger.info("|{:^60}|".format("Información del sistema"))
+        self.logger.info("+{:-^60}+".format(""))
+        self.logger.info("| {:<20} | {:<35} |".format("Nombre de usuario", username))
+        self.logger.info("| {:<20} | {:<35} |".format("Procesador", processor_name))
+        self.logger.info("| {:<20} | {:<35} |".format("Arquitectura", architecture))
+        self.logger.info("| {:<20} | {:<35} |".format("Versión del SO", os_version))
+        self.logger.info("| {:<20} | {:<35} |".format(
+    "Sistema Operativo", operating_system))
+        self.logger.info("+{:-^60}+".format(""))
